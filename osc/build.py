ca42384dce38f7abfbcce38765b4df54f0e85061
Carsten Munk <carsten.munk@gmail.com>
2012-02-10 08:20:27 +0100

    Add sb2install support to osc
    Signed-off-by: Carsten Munk <carsten.munk@gmail.com>

--- osc/build.py
+++ osc/build.py
@@ -165,6 +165,7 @@ class Buildinfo:
             self.pathes.append(node.get('project')+"/"+node.get('repository'))
 
         self.vminstall_list = [ dep.name for dep in self.deps if dep.vminstall ]
+        self.sb2install_list = [ dep.name for dep in self.deps if dep.sb2install ]
         self.preinstall_list = [ dep.name for dep in self.deps if dep.preinstall ]
         self.runscripts_list = [ dep.name for dep in self.deps if dep.runscripts ]
         self.noinstall_list = [ dep.name for dep in self.deps if dep.noinstall ]
@@ -202,6 +203,7 @@ class Pac:
                   'project', 'repository',
                   'preinstall', 'vminstall', 'runscripts',
                   'noinstall', 'installonly', 'notmeta',
+                  'sb2install',
                  ]:
             self.mp[i] = node.get(i)
 
@@ -1127,6 +1129,7 @@ def main(apiurl, opts, argv):
 
     rpmlist.append('preinstall: ' + ' '.join(bi.preinstall_list) + '\n')
     rpmlist.append('vminstall: ' + ' '.join(bi.vminstall_list) + '\n')
+    rpmlist.append('sb2install: ' + ' '.join(bi.sb2install_list) + '\n')
     rpmlist.append('runscripts: ' + ' '.join(bi.runscripts_list) + '\n')
     if build_type != 'kiwi' and build_type != 'docker' and build_type != 'fissile':
         if bi.noinstall_list:
