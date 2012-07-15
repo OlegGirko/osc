6564c75a5fb11596311be30077d3322c79feea3b
David Greaves <david@dgreaves.com>
2012-07-15 22:39:37 +0100

    Add -p to copyprj to enable copying of prjconf

--- osc/core.py
+++ osc/core.py
@@ -3874,6 +3874,7 @@ def copy_prj(src_apiurl: str,
              makeolder: bool = False,
              resign: bool = False,
              now: bool = False,
+             prjconf: bool = False,
              comment: Optional[str] = None):
     """
     Create a copy of a project.
@@ -3895,6 +3896,9 @@ def copy_prj(src_apiurl: str,
         query['comment'] = comment
     if now:
         query['nodelay'] = '1'
+    if prjconf:
+        query['prjconf'] = '1'
+
     u = makeurl(src_apiurl, ['source', dst_project], query=query)
     print("copyprj ", u, file=sys.stderr)
     f = http_POST(u)
