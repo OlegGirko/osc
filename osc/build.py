617b1e9b408ababb2536d55253038002d22d2b91
David Greaves <david@dgreaves.com>
2012-07-27 18:00:25 +0100

    Add support for --rebuild and --chroot-only in build.
    --rebuild sends --skip-prep to build and rpm to skip the %prep phase. This is useful in conjunction with the --rsync* options to enable rapid rebuilds in the clean rpmbuild environment.
    
    --chroot-only creates a chroot without doing a build - this can be used to build scratchbox2 targets

--- osc/build.py
+++ osc/build.py
@@ -654,6 +654,12 @@ def main(apiurl, opts, argv):
         buildargs.append('--norootforbuild')
     if opts.clean:
         buildargs.append('--clean')
+    if opts.rebuild:
+        if not opts.rsyncsrc or not opts.rsyncdest:
+            sys.stderr.write('Warning: --rebuild option is usually used with both --rsync-src and --rsync-dest\n')
+        buildargs.append('--skip-prep')
+    if opts.chroot_only:
+        buildargs.append('--chroot-only')
     if opts.nochecks:
         buildargs.append('--no-checks')
     if not opts.no_changelog:
